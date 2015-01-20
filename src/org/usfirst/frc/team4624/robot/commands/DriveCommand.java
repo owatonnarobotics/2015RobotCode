a8e133e4be8654298dc7ede7b8a523ba1c5d1200
AJGranowski <ajgranowski@gmail.com>
2015-01-20 17:52:56 -0600

    Edited debugging thing Some inputs were missing and all didn't account for more than 1 button push

--- src/org/usfirst/frc/team4624/robot/commands/DriveCommand.java
+++ src/org/usfirst/frc/team4624/robot/commands/DriveCommand.java
@@ -32,17 +32,25 @@ public class DriveCommand extends Command {
 											xboxController.rightStick.y() );
 		
 		// ==DEBUGGING==
-		str += xboxController.a.get() ? "A Button" : "";
-		str += xboxController.b.get() ? "B Button" : "";
-		str += xboxController.back.get() ? "Back Button" : "";
-		str += xboxController.dPad.angle() > 0 ? "DPad" : "";
-		str += xboxController.lb.get() ? "LB Button" : "";
-		str += xboxController.leftStick.y() > .5 ? "Leftstick Y" : "";
-		str += xboxController.lt.get() ? "LT Trigger" : "";
-		str += xboxController.rb.get() ? "RB Button" : "";
-		str += xboxController.start.get() ? "START Button" : "";
-		str += xboxController.x.get() ? "X Button" : "";
-		str += xboxController.y.get() ? "Y Button" : "";
+		str += xboxController.a.get() ? "A Button + " : "";
+		str += xboxController.b.get() ? "B Button + " : "";
+		str += xboxController.back.get() ? "Back Button + " : "";
+		str += xboxController.dPad.angle() > 0 ? "DPad + " : "";
+		str += xboxController.lb.get() ? "LB Button + " : "";
+		str += xboxController.leftStick.y() > .5 ? "Leftstick +Y + " : "";
+		str += xboxController.leftStick.y() < -.5 ? "Leftstick -Y + " : "";
+		str += xboxController.leftStick.x() > .5 ? "Leftstick +X + " : "";
+		str += xboxController.leftStick.x() < -.5 ? "Leftstick -X + " : "";
+		str += xboxController.lt.get() ? "LT Trigger + " : "";
+		str += xboxController.rb.get() ? "RB Button + " : "";
+		str += xboxController.rightStick.y() > .5 ? "Rightstick +Y + " : "";
+		str += xboxController.rightStick.y() < -.5 ? "Rightstick -Y + " : "";
+		str += xboxController.rightStick.x() > .5 ? "Rightstick +X + " : "";
+		str += xboxController.rightStick.x() < -.5 ? "Rightstick -X + " : "";
+		str += xboxController.rt.get() ? "RT Trigger + " : "";
+		str += xboxController.start.get() ? "START Button + " : "";
+		str += xboxController.x.get() ? "X Button + " : "";
+		str += xboxController.y.get() ? "Y Button + " : "";
 		
 		if( str.length() > 0 ) {
 			System.out.println( str );
