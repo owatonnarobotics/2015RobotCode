a1bdde605e5bb7befb63d0a479f0dd6ef5457b58
Michael Pfeifer <mikepf97@gmail.com>
2015-01-30 08:38:25 -0600

    Added and Completed getVector() and Added compressAngle()

--- src/org/usfirst/frc/team4624/robot/input/XboxController.java
+++ src/org/usfirst/frc/team4624/robot/input/XboxController.java
@@ -485,13 +485,17 @@ public final class XboxController extends Joystick {    // Because this class is
 
 
 /* Magnitude */
-public double getAngle(double rawX, double rawY) {
+private double getAngle(double rawX, double rawY) {
     return  Math.toDegrees(Math.atan2(rawY, rawX));
 }
 
-public double getScaleFactor(double angle) { // requires value between 0 and 90
-    if(angle < 0) {
-        angle = Math.abs(angle); // May want to do a while loop that keeps multiplying by 360
+private double compressAngle(double angle) { // Maybe make this a part of getScaleFactor
+    
+}
+
+private double getScaleFactor(double angle) {
+    while(angle < 0) {
+        angle += 360;
     }
     if(angle > 90) {
         angle = angle % 90;
@@ -499,21 +503,33 @@ public double getScaleFactor(double angle) { // requires value between 0 and 90
     return Math.toDegrees(Math.cos(angle)); //This works for any values <= 45
 }
 
-public double getScaleFactorX(double angle, double scaleFactor) {
+private double getScaleFactorX(double angle, double scaleFactor) {
     return  scaleFactor * Math.toDegrees(Math.cos(angle));
 }
-public double getScaleFactorY(double angle, double scaleFactor) {
+private double getScaleFactorY(double angle, double scaleFactor) {
     return scaleFactor * Math.toDegrees(Math.sin(angle));
 }
 
-public double getTrueX(double scaleFactorX, double rawX) {
+private double getTrueX(double scaleFactorX, double rawX) {
     return rawX * scaleFactorX;
 }
 
-public double getTrueY(double scaleFactorY, double rawY) {
+private double getTrueY(double scaleFactorY, double rawY) {
     return rawY * scaleFactorY;
 }
 
-public double getMagnitude(double trueX, double trueY) {
+private double getMagnitude(double trueX, double trueY) {
     return Math.sqrt(Math.pow(trueX, 2) + Math.pow(trueY, 2));
+}
+
+public (something) getVector(double rawX, double rawY) {
+    double angle        = getAngle(rawX, rawY);
+    double smallAngle   = compressAngle(angle);
+    double scaleFactor  = getScaleFactor(smallAngle);
+    double scaleFactorX = getScaleFactorX(angle, scaleFactor);
+    double scaleFactorY = getScaleFactorY(angle, scaleFactor);
+    double trueX        = getTrueX(scaleFactorX, rawX);
+    double trueY        = getTrueY(scaleFactorY, rawY);
+    double magnitude    = getMagnitude(trueX, trueY);
+    return (something);
 }
