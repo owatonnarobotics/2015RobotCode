2ed90c936a210c7e7fad1883c0da23aba909426d
Owatonna Robotics (ThinkPad) <anonymous@student.owatonna.k12.mn.us>
2015-02-05 18:29:31 -0600

    Started height detection

--- src/org/usfirst/frc/team4624/robot/subsystems/Forklift.java
+++ src/org/usfirst/frc/team4624/robot/subsystems/Forklift.java
@@ -1,10 +1,15 @@
 package org.usfirst.frc.team4624.robot.subsystems;
 
+import edu.wpi.first.wpilibj.CANJaguar;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Forklift extends Subsystem {
 
+    CANJaguar liftParent;
+    CANJaguar liftChild;
+    
     public Forklift() {
+        //liftParent = new CANJaguar()
     }
     
     public void stop() {
@@ -22,6 +27,13 @@ public class Forklift extends Subsystem {
 
     public void changeHeight(int changeHeight) {
         // TODO Auto-generated method stub
-        
+    }
+    
+    public double getHeight(double curveDistance) {
+        return (27.7 - 38 * Math.toDegrees(Math.cos(43.2 + (360 * curveDistance) / (76 * Math.PI))));
+    }
+    
+    public double distanceToRevolutions(double distance) {
+        return distance / 2.5;
     }
 }
