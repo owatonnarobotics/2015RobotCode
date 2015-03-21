38395170c62a5c0bd44efc00121b1468c9027495
Michael Pfeifer <mikepf97@gmail.com>
2015-03-20 23:04:37 -0500

    Cleaned autonomous a little

--- src/org/usfirst/frc/team4624/robot/autonomous/Autonomous.java
+++ src/org/usfirst/frc/team4624/robot/autonomous/Autonomous.java
@@ -5,10 +5,10 @@ package org.usfirst.frc.team4624.robot.autonomous;
 import org.usfirst.frc.team4624.robot.commands.*;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
-
-
 public class Autonomous extends CommandGroup { //TODO Move this class into commands folder ???
 
+    private int arms;
+    
     public Autonomous(int location, int goal, int rotation, int type, int arms) {
     
         /*
@@ -29,10 +29,11 @@ public class Autonomous extends CommandGroup { //TODO Move this class into comma
          * 
          * Arms = 0
          * No Arms = 1
-         * 
          */
         
-         final double FOOT_ISH = .29; //give or take a lot... ish
+        this.arms = arms;
+        
+        final double FOOT_ISH = .29; //give or take a lot... ish
         
         if (type == 0 || type == 1) {
             
@@ -50,47 +51,35 @@ public class Autonomous extends CommandGroup { //TODO Move this class into comma
             }
             
             if (goal == 0) { //Bin
-                if(arms != 1) {
-                    addSequential(new ReleaseArms());
-                }
+                releaseArms();
                 addSequential(new AutoLift(2));
                 pause(2.5);
                 addSequential(new AutoDrive(smallTime, forwardSpeed));
                 pause(.8);
-                if(arms != 1) {
-                    addSequential(new GrabArms());
-                }
+                grabArms();
                 pause(.25);
                 addSequential(new AutoLift(3));
                 pause(.25);
                 addSequential(new AutoDrive(actionTime, -speed));
                 addSequential(new AutoLift(2));
                 pause(.75);
-                if(arms != 1) {
-                    addSequential(new ReleaseArms());
-                }
+                releaseArms();
                 pause(.5);
                 addSequential(new AutoLift(4));
                 pause(2.5);
             }
             
             else if (goal == 1) { //Tote
-                if(arms != 1) {
-                    addSequential(new ReleaseArms());
-                }
+                releaseArms();
                 addSequential(new AutoDrive(smallTime, forwardSpeed));
                 pause(.8);
-                if(arms != 1) {
-                    addSequential(new GrabArms());
-                }
+                grabArms();
                 addSequential(new AutoLift(1));
                 pause(1.0);
                 addSequential(new AutoDrive(actionTime, -speed));
                 addSequential(new AutoLift(0));
                 pause(1.2);
-                if(arms != 1) {
-                    addSequential(new ReleaseArms());
-                }
+                releaseArms();
                 addSequential(new AutoLift(3));
                 pause(1.0);
             }
@@ -110,50 +99,61 @@ public class Autonomous extends CommandGroup { //TODO Move this class into comma
             
             pause(1);
             addSequential(new AutoLift(0));
-            if((!(rotation == 2)) && (arms != 1)) {
+            if ((!(rotation == 2)) && (arms != 1)) {
                 addSequential(new GrabArms());
             }
         }
         
         // Should be 1, switched to two so it doesn't happen
         else if (type == 2) {
-           addSequential(new AutoDriveAdvanced(.1, 0, FOOT_ISH / 12));
-           pause(0.1);
-           addSequential(new GrabArms());
-           pause(0.1);
-           addSequential(new AutoLift(2));
-           pause(0.25);
-           addSequential(new AutoDriveAdvanced(2, 0, 6.3 * FOOT_ISH));
-           pause(2.0);
-           addSequential(new ReleaseArms());
-           addSequential(new AutoLift(0));
-           pause(.2);
-           addSequential(new GrabArms());
-           pause(.1);
-           addSequential(new AutoLift(2));
-           pause(.1);
-           addSequential(new AutoDriveAdvanced(2, 0, 6.3 * FOOT_ISH));
-           pause(2.0);
-           addSequential(new ReleaseArms());
-           addSequential(new AutoLift(0));
-           pause(.2);
-           addSequential(new GrabArms());
-           pause(.1);
-           addSequential(new AutoLift(2));
-           pause(.1);
-           addSequential(new AutoDriveAdvanced(6, 9.75 * FOOT_ISH, 0));
-           pause(6);
-           addSequential(new AutoLift(0));
-           pause(.1);
-           addSequential(new ReleaseArms());
-           pause(.1);
-           addSequential(new AutoDriveAdvanced(.5, 0, .5 * FOOT_ISH));
-           pause(.5);
+            addSequential(new AutoDriveAdvanced(.1, 0, FOOT_ISH / 12));
+            pause(0.1);
+            addSequential(new GrabArms());
+            pause(0.1);
+            addSequential(new AutoLift(2));
+            pause(0.25);
+            addSequential(new AutoDriveAdvanced(2, 0, 6.3 * FOOT_ISH));
+            pause(2.0);
+            addSequential(new ReleaseArms());
+            addSequential(new AutoLift(0));
+            pause(.2);
+            addSequential(new GrabArms());
+            pause(.1);
+            addSequential(new AutoLift(2));
+            pause(.1);
+            addSequential(new AutoDriveAdvanced(2, 0, 6.3 * FOOT_ISH));
+            pause(2.0);
+            addSequential(new ReleaseArms());
+            addSequential(new AutoLift(0));
+            pause(.2);
+            addSequential(new GrabArms());
+            pause(.1);
+            addSequential(new AutoLift(2));
+            pause(.1);
+            addSequential(new AutoDriveAdvanced(6, 9.75 * FOOT_ISH, 0));
+            pause(6);
+            addSequential(new AutoLift(0));
+            pause(.1);
+            addSequential(new ReleaseArms());
+            pause(.1);
+            addSequential(new AutoDriveAdvanced(.5, 0, .5 * FOOT_ISH));
+            pause(.5);
         }
     }
     
     private void pause(double time) {
-    
         addSequential(new AutoPause(time));
+    }
+    
+    private void grabArms() {
+        if (arms != 1) {
+            addSequential(new GrabArms());
+        }
+    }
+    
+    private void releaseArms() {
+        if (arms != 1) {
+            addSequential(new ReleaseArms());
+        }
     }
 }
