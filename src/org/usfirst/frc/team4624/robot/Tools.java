513b2a1206228e8dbd5ee8eedd7c13e7dc15cb81
AJGranowski <ajgranowski@gmail.com>
2015-05-12 10:58:02 -0500

    Added Levenshtein distance to difference methods in Tools class

--- src/org/usfirst/frc/team4624/robot/Tools.java
+++ src/org/usfirst/frc/team4624/robot/Tools.java
@@ -139,6 +139,30 @@ public class Tools {
         return (short) Math.abs(a - b);
     }
     
+    /**
+     * Find the difference between two strings
+     * @param a
+     * @param b
+     * @return the Levenshtein distance between the two strings
+     */
+    public static int difference(String a, String b) {
+        int[][] distance = new int[a.length() + 1][b.length() + 1];        
+        
+        for (int i = 0; i <= a.length(); i++)                                 
+            distance[i][0] = i;                                                  
+        for (int j = 1; j <= b.length(); j++)                                 
+            distance[0][j] = j;                                                  
+ 
+        for (int i = 1; i <= a.length(); i++)                                 
+            for (int j = 1; j <= b.length(); j++)                             
+                distance[i][j] =    Math.min(Math.min(
+                        distance[i - 1][j] + 1,
+                        distance[i][j - 1] + 1),
+                        distance[i - 1][j - 1] + ((a.charAt(i - 1) == b.charAt(j - 1)) ? 0 : 1));
+ 
+        return distance[a.length()][b.length()];
+    }
+    
     
     
 }
