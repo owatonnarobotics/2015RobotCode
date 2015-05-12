d309f3a0b273f1c0ba688cb3973762ce79c1106f
AJGranowski <ajgranowski@gmail.com>
2015-05-12 10:49:20 -0500

    Resorted clamp functions

--- src/org/usfirst/frc/team4624/robot/Tools.java
+++ src/org/usfirst/frc/team4624/robot/Tools.java
@@ -18,41 +18,8 @@ public class Tools {
      * @param max
      * @return clamped value
      */
-    public static int clamp(int value, int min, int max) {
-        return Math.max(min, Math.min(max, value));
-    }
-    
-    /**
-     * Clamp a value between two different values
-     * @param value
-     * @param min
-     * @param max
-     * @return clamped value
-     */
-    public static short clamp(short value, short min, short max) {
-        return (short) Math.max(min, Math.min(max, value));
-    }
-    
-    /**
-     * Clamp a value between two different values
-     * @param value
-     * @param min
-     * @param max
-     * @return clamped value
-     */
     public static byte clamp(byte value, byte min, byte max) {
         return (byte) Math.max(min, Math.min(max, value));
-    }
-    
-    /**
-     * Clamp a value between two different values
-     * @param value
-     * @param min
-     * @param max
-     * @return clamped value
-     */
-    public static float clamp(float value, float min, float max) {
-        return Math.max(min, Math.min(max, value));
     }
     
     /**
@@ -73,8 +40,41 @@ public class Tools {
      * @param max
      * @return clamped value
      */
+    public static float clamp(float value, float min, float max) {
+        return Math.max(min, Math.min(max, value));
+    }
+    
+    /**
+     * Clamp a value between two different values
+     * @param value
+     * @param min
+     * @param max
+     * @return clamped value
+     */
+    public static int clamp(int value, int min, int max) {
+        return Math.max(min, Math.min(max, value));
+    }
+    
+    /**
+     * Clamp a value between two different values
+     * @param value
+     * @param min
+     * @param max
+     * @return clamped value
+     */
     public static long clamp(long value, long min, long max) {
         return Math.max(min, Math.min(max, value));
+    }
+    
+    /**
+     * Clamp a value between two different values
+     * @param value
+     * @param min
+     * @param max
+     * @return clamped value
+     */
+    public static short clamp(short value, short min, short max) {
+        return (short) Math.max(min, Math.min(max, value));
     }
     
     
