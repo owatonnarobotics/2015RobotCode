6819254b62e23c1fb7472d850e962a497e94c71c
AJGranowski <ajgranowski@gmail.com>
2015-05-14 10:35:16 -0500

    Cleaned up ExampleSubsystem class

--- src/org/usfirst/frc/team4624/robot/templates/ExampleSubsystem.java
+++ src/org/usfirst/frc/team4624/robot/templates/ExampleSubsystem.java
@@ -1,19 +1,25 @@
-
 package org.usfirst.frc.team4624.robot.templates;
 
+
+
 import edu.wpi.first.wpilibj.command.Subsystem;
+
+
 
 /**
  *
  */
 public class ExampleSubsystem extends Subsystem {
     
+    
+    
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
-
+    
+    @Override
     public void initDefaultCommand() {
+    
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
     }
 }
-
