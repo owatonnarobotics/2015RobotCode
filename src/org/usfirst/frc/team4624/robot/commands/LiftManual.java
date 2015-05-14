ed6afee94f5d3dca933bef193c5272a8505b8867
AJGranowski <ajgranowski@gmail.com>
2015-05-14 10:24:55 -0500

    Cleaned up LiftManual class

--- src/org/usfirst/frc/team4624/robot/commands/LiftManual.java
+++ src/org/usfirst/frc/team4624/robot/commands/LiftManual.java
@@ -1,42 +1,55 @@
 package org.usfirst.frc.team4624.robot.commands;
 
+
+
 import org.usfirst.frc.team4624.robot.OI;
 import org.usfirst.frc.team4624.robot.Robot;
 import org.usfirst.frc.team4624.robot.RobotMap;
-
 import edu.wpi.first.wpilibj.command.Command;
 
-public class LiftManual extends Command {
 
+
+public class LiftManual extends Command {
+    
+    
+    
     public LiftManual() {
+    
         requires(Robot.forklift);
     }
-
+    
+    @Override
+    protected void end() {
+    
+    }
+    
+    @Override
+    protected void execute() {
+    
+        Robot.forklift
+                .setRate((OI.xboxController.rt.getX() - OI.xboxController.lt
+                        .getX()) * RobotMap.MANUAL_LIFT_SPEED);
+    }
+    
     @Override
     protected void initialize() {
-        if(OI.xboxController.lt.getX() != 0 || OI.xboxController.rt.getX() !=0) {
+    
+        if (OI.xboxController.lt.getX() != 0
+                || OI.xboxController.rt.getX() != 0) {
             Robot.forklift.setManualMode();
         }
     }
-
-    @Override
-    protected void execute() {
-        Robot.forklift.setRate((OI.xboxController.rt.getX() -
-                                OI.xboxController.lt.getX()) * RobotMap.MANUAL_LIFT_SPEED);
-    }
-
-    @Override
-    protected boolean isFinished() {
-        return true;
-    }
-
-    @Override
-    protected void end() {
-    }
-
+    
     @Override
     protected void interrupted() {
+    
         end();
     }
-
+    
+    @Override
+    protected boolean isFinished() {
+    
+        return true;
+    }
+    
 }
