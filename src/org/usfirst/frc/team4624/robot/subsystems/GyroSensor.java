8cde0f3bf6cdc0421b7ba4832930fdb7134d4094
AJGranowski <ajgranowski@gmail.com>
2015-05-14 10:29:52 -0500

    Cleaned up GyroSensor class

--- src/org/usfirst/frc/team4624/robot/subsystems/GyroSensor.java
+++ src/org/usfirst/frc/team4624/robot/subsystems/GyroSensor.java
@@ -1,29 +1,40 @@
 package org.usfirst.frc.team4624.robot.subsystems;
 
+
+
 import org.usfirst.frc.team4624.robot.RobotMap;
 import edu.wpi.first.wpilibj.Gyro;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 
-public class GyroSensor extends Subsystem {
 
+public class GyroSensor extends Subsystem {
+    
+    
+    
     Gyro gyroSensor;
     
+    
+    
     public GyroSensor() {
+    
         gyroSensor = new Gyro(RobotMap.GYRO);
         gyroSensor.initGyro();
     }
     
     public double getAngle() {
-        return gyroSensor.getAngle();
-    }
     
-    public void reset() {
-        gyroSensor.reset();
+        return gyroSensor.getAngle();
     }
     
     @Override
     protected void initDefaultCommand() {
+    
+    }
+    
+    public void reset() {
+    
+        gyroSensor.reset();
     }
     
 }
