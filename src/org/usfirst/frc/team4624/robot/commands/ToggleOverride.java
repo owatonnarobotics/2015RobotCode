3dcf67cb1148153b1445148e7c628e95103d8adb
AJGranowski <ajgranowski@gmail.com>
2015-05-14 10:27:49 -0500

    Cleaned up ToggleOverride class

--- src/org/usfirst/frc/team4624/robot/commands/ToggleOverride.java
+++ src/org/usfirst/frc/team4624/robot/commands/ToggleOverride.java
@@ -1,37 +1,48 @@
 package org.usfirst.frc.team4624.robot.commands;
 
+
+
 import org.usfirst.frc.team4624.robot.Robot;
 import edu.wpi.first.wpilibj.command.Command;
 
 
-public class ToggleOverride extends Command {
 
+public class ToggleOverride extends Command {
+    
+    
+    
     public ToggleOverride() {
+    
         requires(Robot.forklift);
     }
-    @Override
-    protected void initialize() {
-    }
-
-    @Override
-    protected void execute() {
-        Robot.forklift.toggleOverride();
-    }
-
-    @Override
-    protected boolean isFinished() {
-        return true;
-    }
-
+    
     @Override
     protected void end() {
+    
     }
-
+    
+    @Override
+    protected void execute() {
+    
+        Robot.forklift.toggleOverride();
+    }
+    
+    @Override
+    protected void initialize() {
+    
+    }
+    
     @Override
     protected void interrupted() {
     
         // TODO Auto-generated method stub
         
+    }
+    
+    @Override
+    protected boolean isFinished() {
+    
+        return true;
     }
     
 }
