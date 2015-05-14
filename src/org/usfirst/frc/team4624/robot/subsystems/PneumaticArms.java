d84a8d45307bf4550c658e827af8f23e52b54245
AJGranowski <ajgranowski@gmail.com>
2015-05-14 10:31:41 -0500

    Cleaned up PneumaticArms class

--- src/org/usfirst/frc/team4624/robot/subsystems/PneumaticArms.java
+++ src/org/usfirst/frc/team4624/robot/subsystems/PneumaticArms.java
@@ -1,17 +1,26 @@
 package org.usfirst.frc.team4624.robot.subsystems;
 
+
+
 import org.usfirst.frc.team4624.robot.RobotMap;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-public class PneumaticArms extends Subsystem {
 
+
+public class PneumaticArms extends Subsystem {
+    
+    
+    
     Solenoid arms;
+    
+    
     
     /**
      * Initializes PneumaticArms subsystem. Should only be called once.
      */
     public PneumaticArms() {
+    
         arms = new Solenoid(RobotMap.PCM_CAN_ID, RobotMap.PCM_SOLENOID_PORT);
         grab();
     }
@@ -20,17 +29,20 @@ public class PneumaticArms extends Subsystem {
      * When run, causes the arms of the robot to close.
      */
     public void grab() {
+    
         arms.set(false);
+    }
+    
+    @Override
+    protected void initDefaultCommand() {
+    
     }
     
     /**
      * When run, causes the arms of the robot to open.
      */
     public void release() {
+    
         arms.set(true);
-    }
-
-    @Override
-    protected void initDefaultCommand() {        
     }
 }
