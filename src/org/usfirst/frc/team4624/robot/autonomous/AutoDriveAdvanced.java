45608632b678b7149892b668690a9499e2798899
AJGranowski <ajgranowski@gmail.com>
2015-05-14 10:16:30 -0500

    Cleaned up AutoDriveAdvanced class

--- src/org/usfirst/frc/team4624/robot/autonomous/AutoDriveAdvanced.java
+++ src/org/usfirst/frc/team4624/robot/autonomous/AutoDriveAdvanced.java
@@ -1,46 +1,56 @@
 package org.usfirst.frc.team4624.robot.autonomous;
 
+
+
 import org.usfirst.frc.team4624.robot.Robot;
 import edu.wpi.first.wpilibj.command.Command;
 
 
-public class AutoDriveAdvanced extends Command{
 
+public class AutoDriveAdvanced extends Command {
+    
+    
+    
     private double time;
     private double distX;
     private double distY;
     
-    public AutoDriveAdvanced(double time, double distX, double distY){
-        
+    
+    
+    public AutoDriveAdvanced(double time, double distX, double distY) {
+    
         this.time = time;
         this.distX = distX;
         this.distY = distY;
         requires(Robot.powertrain);
         this.setTimeout(time);
     }
-    @Override
-    protected void initialize() {
-        
-    }
-
-    @Override
-    protected void execute() { 
-       Robot.powertrain.move(distX / time, distY / time, 0);
-    }
-
-    @Override
-    protected boolean isFinished() {
-        return this.isTimedOut();
-    }
-
+    
     @Override
     protected void end() {
-        
+    
     }
-
+    
+    @Override
+    protected void execute() {
+    
+        Robot.powertrain.move(distX / time, distY / time, 0);
+    }
+    
+    @Override
+    protected void initialize() {
+    
+    }
+    
     @Override
     protected void interrupted() {
-        
+    
+    }
+    
+    @Override
+    protected boolean isFinished() {
+    
+        return this.isTimedOut();
     }
     
 }
